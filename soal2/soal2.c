34ee197a321975df717bb53abff0a144e6584c1b
dydyandra <dy.dyandraparamitha@gmail.com>
2021-04-14 10:55:36 +0700

    2a done

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -20,19 +20,19 @@ void execute(char script[], char *argv[]){
 
 void unzipFiles(){
     int signal;
-    pid_t child;
-    child=fork();
+    pid_t child_id;
+    child_id=fork();
 
-    if (child < 0){
+    if (child_id < 0){
         exit(EXIT_FAILURE);
     }
 
-    if (child==0){
-        char *argv[]={"mkdir", "-p", "/home/dyandra/modul2/petshop", NULL};
-        execute("/bin/mkdir", argv);
+    if (child_id==0){
+        char *mkPetshop[]={"make-directory", "-p", "/home/dyandra/modul2/petshop", NULL};
+        execute("/bin/mkdir", mkPetshop);
 
-        char *argv2[]={"unzipping", "-d", "/home/dyandra/modul2/petshop", "/home/dyandra/Downloads/pets.zip", NULL};
-        execute("/bin/unzip", argv2);
+        char *unzipPets[]={"unzipping", "-d", "/home/dyandra/modul2/petshop", "/home/dyandra/Downloads/pets.zip", NULL};
+        execute("/bin/unzip", unzipPets);
 
         DIR *directory;
         directory=opendir("/home/dyandra/modul2/petshop");
@@ -42,8 +42,8 @@ void unzipFiles(){
                  if(infolder->d_type == DT_DIR){
                      char base[100] = "/home/dyandra/modul2/petshop/";
                      strcat(base, infolder->d_name);
-                     char *argv3[] = {"rm", "-r", "-f", base, NULL};
-                     execute("/bin/rm", argv3);
+                     char *listPets[] = {"rm", "-r", "-f", base, NULL};
+                     execute("/bin/rm", listPets);
                  }
              }
 
